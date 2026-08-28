04bc077ef30ba2764be353256cdead75c4031c31
Μιθριδάτης <game.developer.mb@gmail.com>
2026-08-28 12:30:18 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,4 @@
-const CACHE_NAME = 'cabinet-workshop-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.json',
-  '/assets/icons/icon-192.png',
-  '/assets/icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+const CACHE = 'cabinet-v1';
+const ASSETS = ['/', '/index.html','/styles.css','/app.js','/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
